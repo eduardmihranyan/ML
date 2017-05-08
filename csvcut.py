0da3093286b1967542438f4b40dc3d5692384c27
AnushTosunyan <anushtosunyan@gmail.com>
2017-05-08 16:16:27 +0400

    commited by Anush

---
+++ csvcut.py
@@ -0,0 +1,103 @@
+from argparse import ArgumentParser
+import sys
+
+
+DESCRIPTION = 'csvpp - prints cvs file in human-readable format'
+EXAMPLES = 'example: cat file.txt | csvpp -f | less -SR'
+
+
+def write_row(index ,row, output_stream):
+    """
+    Write_row modifeis the rows, leaving only the column desired
+    :param index: the indeces of columns that need to remain in the file
+    :param row: row represented as a list of columns
+    :param output_stream: a stream of modified row
+
+    """
+    output_line = ''
+    for i, column in enumerate(row):
+        if(i in index):
+            if(i == max(index)):
+                output_line += column + ""
+            elif(i < max(index)):
+                output_line += column  + ','
+    
+    output_line += '\n'
+    output_stream.write(output_line)
+
+
+def main():
+    args = parse_args()
+    input_stream = open(args.file, 'r') if args.file else sys.stdin
+    output_stream = open(args.output_file, 'r') if args.output_file else sys.stdout
+
+    columns = input_stream.readline().strip().split(args.separator)
+    first_rows = [columns]
+    
+    index = []
+    fields_int = []
+
+    if args.fields == 'None':
+        fields = first_rows[0]
+    elif len(args.fields) == 0:
+        fields = []
+    else:
+        fields = args.fields[0].strip().split(args.separator)
+    
+        if ((fields[0]).isdigit()):
+            for i in range (len(fields)):
+                fields_int.append(int(fields[i]))
+
+
+
+    c = list(range(len(columns)))
+    unique_l = list(set(columns))
+    unique_c = [columns.index(x) for x in unique_l]
+
+    for i,row in enumerate(first_rows):
+        if(i == 0):
+            if len(args.fields) == 0:
+                index = []
+            elif ((fields[0]).isdigit()):
+                index = fields_int
+            else:
+                for j, column in enumerate(row):
+                    if(column in fields):
+                        index.append(j)
+
+        if args.complement:
+            index = [x for x in c if x not in index]
+        
+        if args.unique:
+            index = [x for x in unique_c if x in index]
+
+        write_row(index, row, output_stream)
+
+    for row in input_stream:
+        write_row(index, row.strip().split(args.separator), output_stream)
+   
+       
+    if input_stream != sys.stdin:
+        input_stream.close()
+    if output_stream != sys.stdout:
+        output_stream.close()
+
+def parse_args():
+    parser = ArgumentParser(description=DESCRIPTION, epilog=EXAMPLES)
+
+    parser.add_argument('-s', '--separator', type=str, help='Separator to be used', default=',')
+    parser.add_argument('-o', '--output_file', type=str, help='Output file. stdout is used by default')
+    parser.add_argument('-f', '--fields', nargs = '*', type=str, 
+        help='fields to be left, please give the fields with the seperator, same as the dataset, and withou any spaces', 
+        default = 'None')
+    parser.add_argument('-c', '--complement', help = 'leaves only the complement', action = 'store_true')
+    parser.add_argument('-u', '--unique', help = 'Remove duplicates from list of FIELDS', action = 'store_true')    
+    
+
+    parser.add_argument('file', nargs='?', help='File to read input from. stdin is used by default')
+
+    args = parser.parse_args()
+
+    return args
+
+main()
