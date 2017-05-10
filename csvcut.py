a7135d1f81a9b1998082774db9cbb8f7c04dcf17
AnushTosunyan <anushtosunyan@gmail.com>
2017-05-10 10:48:48 +0400

    updated

--- csvcut.py
+++ csvcut.py
@@ -1,12 +1,13 @@
+ #!/usr/bin/env python
 from argparse import ArgumentParser
 import sys
 
 
-DESCRIPTION = 'csvpp - prints cvs file in human-readable format'
-EXAMPLES = 'example: cat file.txt | csvpp -f | less -SR'
+DESCRIPTION = 'csvcut - returns cvs file with only desired columns'
+EXAMPLES = 'example: cat file.txt | csvcut -f "col_name"| less -SR'
 
 
-def write_row(index ,row, output_stream):
+def write_row(index ,row, output_stream, separator = ','):
     """
     Write_row modifeis the rows, leaving only the column desired
     :param index: the indeces of columns that need to remain in the file
@@ -15,14 +16,14 @@ def write_row(index ,row, output_stream):
 
     """
     output_line = ''
+
+    mi = 0 if index==[] else max(index) 
+
     for i, column in enumerate(row):
-        if(i in index):
-            if(i == max(index)):
-                output_line += column + ""
-            elif(i < max(index)):
-                output_line += column  + ','
-    
-    output_line += '\n'
+        if i not in index:
+            continue
+        output_line += column + separator if i < mi else  column + '\n' 
+        
     output_stream.write(output_line)
 
 
@@ -37,10 +38,10 @@ def main():
     index = []
     fields_int = []
 
-    if args.fields == 'None':
-        fields = first_rows[0]
-    elif len(args.fields) == 0:
+    if len(args.fields) == 0:
         fields = []
+    elif args.fields == []:
+        fields = columns
     else:
         fields = args.fields[0].strip().split(args.separator)
     
@@ -55,7 +56,7 @@ def main():
     unique_c = [columns.index(x) for x in unique_l]
 
     for i,row in enumerate(first_rows):
-        if(i == 0):
+        if i == 0:
             if len(args.fields) == 0:
                 index = []
             elif ((fields[0]).isdigit()):
@@ -71,10 +72,10 @@ def main():
         if args.unique:
             index = [x for x in unique_c if x in index]
 
-        write_row(index, row, output_stream)
+        write_row(index, row, output_stream, separator = args.separator)
 
     for row in input_stream:
-        write_row(index, row.strip().split(args.separator), output_stream)
+        write_row(index, row.strip().split(args.separator), output_stream, separator = args.separator)
    
        
     if input_stream != sys.stdin:
@@ -88,8 +89,7 @@ def parse_args():
     parser.add_argument('-s', '--separator', type=str, help='Separator to be used', default=',')
     parser.add_argument('-o', '--output_file', type=str, help='Output file. stdout is used by default')
     parser.add_argument('-f', '--fields', nargs = '*', type=str, 
-        help='fields to be left, please give the fields with the seperator, same as the dataset, and withou any spaces', 
-        default = 'None')
+        help='fields to be left, please give the fields with the seperator, same as the dataset, and withou any spaces')
     parser.add_argument('-c', '--complement', help = 'leaves only the complement', action = 'store_true')
     parser.add_argument('-u', '--unique', help = 'Remove duplicates from list of FIELDS', action = 'store_true')    
     
